3a04f72c426f6eaaed13d7f0837394bb83190d96
kauedelazzeri <kauedelazzeri@gmail.com>
2025-05-11 19:10:30 -0300

    Initial commit

---
+++ my-portfolio/src/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="p-4 text-lg font-bold border-b border-gray-300">
+      Welcome to my PM Portfolio
+    </header>
+  );
+}
---
+++ my-portfolio/src/pages/page.tsx
@@ -0,0 +1,10 @@
+import Header from "@/components/Header";
+
+export default function Home() {
+  return (
+    <>
+      <Header />
+      <main className="p-4">Your portfolio content goes here.</main>
+    </>
+  );
+}
