d2590eea9ef15be467aef1b66fe58aadb2d4b4f2
kauedelazzeri <kauedelazzeri@gmail.com>
2025-05-11 21:18:07 -0300

    fix: vercel configurations

--- my-portfolio/next.config.js
+++ my-portfolio/next.config.js
@@ -1,6 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: '**',
+      },
+    ],
+  },
+  experimental: {
+    optimizeCss: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
 }
 
 module.exports = nextConfig
